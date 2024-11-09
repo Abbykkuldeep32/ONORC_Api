e8e21b80de0b483c1d7b8a566f05c7ffc629eb88
Kuldeep Kumar <abbykkuldeep32@gmail.com>
2024-11-09 16:37:32 +0530

    dealer update

--- src/main/java/com/example/dealer/controller/DealerController.java
+++ src/main/java/com/example/dealer/controller/DealerController.java
@@ -96,7 +96,7 @@ public class DealerController {
 	        response.put("status", true);
 	        response.put("message", "OTP verified successfully");
 	        response.put("token", token);
-	        response.put("data", fps);
+	        response.put("dealer", fps);
 	        tempStoreService.clearFpsData(otpRequest.getMobileNo());
 	        return ResponseEntity.ok(response);
 	    } else {
