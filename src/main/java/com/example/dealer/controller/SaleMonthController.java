2c27f464a78b16b8a65f799e98abb33e82dd8fc2
Kuldeep Kumar <abbykkuldeep32@gmail.com>
2024-11-29 16:19:20 +0530

    SaleMonth Controller update

--- src/main/java/com/example/dealer/controller/SaleMonthController.java
+++ src/main/java/com/example/dealer/controller/SaleMonthController.java
@@ -9,6 +9,7 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -24,7 +25,7 @@ public class SaleMonthController {
 	@Autowired
     private SaleMonthService salemonthService;
 	
-	@GetMapping("/sale")
+	@PostMapping("/sale")
     public ResponseEntity<Map<String, Object>> getSaleByFpsidAndMonth(@RequestBody SaleMonth salemonth){
     	
     	List<SaleMonth> salesm= salemonthService.getSaleByFpsidAndMonth(salemonth.getFpsId(),salemonth.getAllocationMonth(),salemonth.getAllocationYear());
