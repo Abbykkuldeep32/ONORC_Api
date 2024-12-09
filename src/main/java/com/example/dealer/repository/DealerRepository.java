7b1d27998581239f7d8e465879aa9b702a312907
Kuldeep Kumar <abbykkuldeep32@gmail.com>
2024-12-09 16:23:30 +0530

    application update

--- src/main/java/com/example/dealer/repository/DealerRepository.java
+++ src/main/java/com/example/dealer/repository/DealerRepository.java
@@ -8,7 +8,6 @@ import org.springframework.data.repository.query.Param;
 
 import com.example.dealer.model.Dealer;
 
-
 public interface DealerRepository extends JpaRepository<Dealer,String> {
 
 	@Query("SELECT s FROM Dealer s WHERE s.statename = :statename AND s.mobile_no = :mobile_no")
