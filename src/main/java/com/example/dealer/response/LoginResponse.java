d28f4dd0c74aa20a71c2cf1887b8f570918ed7dc
Kuldeep Kumar <abbykkuldeep32@gmail.com>
2024-12-18 12:44:45 +0530

    mpin controller

--- src/main/java/com/example/dealer/response/LoginResponse.java
+++ src/main/java/com/example/dealer/response/LoginResponse.java
@@ -7,48 +7,40 @@ import com.example.dealer.model.Dealer;
 public class LoginResponse {
 	private boolean status;
 	private String message;
-    private List<Dealer> dealer;
+    private List<Dealer> data;
     private String token;
-
-    // Getters and Setters
-    public boolean isStatus() {
-        return status;
-    }
-
-    public void setStatus(boolean status) {
-        this.status = status;
-    }
-    
-    public String getMessage() {
-        return message;
-    }
-
-    public void setMessage(String message) {
-        this.message = message;
-    }
-
-	public List<Dealer> getDealer() {
-		return dealer;
+	public boolean isStatus() {
+		return status;
 	}
-
-	public void setDealer(List<Dealer> dealer) {
-		this.dealer = dealer;
+	public void setStatus(boolean status) {
+		this.status = status;
 	}
-	
-
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public List<Dealer> getData() {
+		return data;
+	}
+	public void setData(List<Dealer> data) {
+		this.data = data;
+	}
 	public String getToken() {
 		return token;
 	}
-
 	public void setToken(String token) {
 		this.token = token;
 	}
-
-	public LoginResponse(boolean status, String message, List<Dealer> dealer, String token) {
+	public LoginResponse(boolean status, String message, List<Dealer> data, String token) {
 		super();
 		this.status = status;
 		this.message = message;
-		this.dealer = dealer;
+		this.data = data;
 		this.token = token;
 	}
+
+    // Getters and Setters
+    
 }
