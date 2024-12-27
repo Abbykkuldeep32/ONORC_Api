b5e9e672642fe57149175d2224284688a65e19db
Kuldeep Kumar <abbykkuldeep32@gmail.com>
2024-12-27 16:27:45 +0530

    security config

--- src/main/java/com/example/dealer/config/SecurityConfig.java
+++ src/main/java/com/example/dealer/config/SecurityConfig.java
@@ -21,21 +21,42 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    	 http
-    	 .headers()
-         .xssProtection()
-         .and()
-         .contentSecurityPolicy("default-src 'self'; script-src 'self'; object-src 'none';")
-         .and()
-         .frameOptions().sameOrigin()
-         .and()
-         .cors().and().csrf().disable()
-         .authorizeHttpRequests()
-             .requestMatchers("/public/**").permitAll()
-             .requestMatchers("/api/v1/saledetail/generate-receipt/**").permitAll()
-             .anyRequest().authenticated()
-             .and()
-    	 .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+        http
+            .headers()
+                .xssProtection()
+                .and()
+                .contentSecurityPolicy("default-src 'self'; script-src 'self'; object-src 'none'; img-src 'self'; style-src 'self';")
+                .and()
+                .httpStrictTransportSecurity()
+                    .includeSubDomains(true)
+                    .maxAgeInSeconds(31536000)
+                    .preload(true)
+                .and()
+                .frameOptions().sameOrigin()
+                .and()
+            .cors()
+            .and()
+            .csrf().disable()  // Ensure CSRF is disabled only if you're using JWT or other secure methods
+            .authorizeHttpRequests()
+                .requestMatchers("/public/**").permitAll()
+                .requestMatchers("/api/v1/saledetail/generate-receipt/**").permitAll()
+                .anyRequest().authenticated()
+                .and()
+            .addFilterBefore(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
         return http.build();
     }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.addAllowedOrigin("http://103.25.130.105:9090"); // Allowed origin
+        corsConfiguration.addAllowedMethod("*"); // Allow all HTTP methods
+        //corsConfiguration.addAllowedHeader("*"); // Allow all headers (if necessary)
+        corsConfiguration.setAllowCredentials(true); // Allow credentials (cookies, HTTP authentication)
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration); // Apply globally
+        return source;
+    }
+
 }
