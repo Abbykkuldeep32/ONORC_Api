e1ebde0fc8e507a2cc454f7228dd3784762ea292
Kuldeep Kumar <abbykkuldeep32@gmail.com>
2024-12-27 17:10:31 +0530

    stock controller

--- src/main/java/com/example/dealer/dfso/controller/StockController.java
+++ src/main/java/com/example/dealer/dfso/controller/StockController.java
@@ -4,6 +4,7 @@ import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,7 +20,7 @@ public class StockController {
 	@Autowired
     private StockService stockService;
 
-    @GetMapping("/totalStockQuantity")
+    @PostMapping("/totalStockQuantity")
     public Map<String, Object> getTotalStock(@RequestBody FpsStockBalance fpsstock) {
         return stockService.getTotalStock(fpsstock.getStatecode(),fpsstock.getAllocation_month(),fpsstock.getAllocation_year(),fpsstock.getFpscode());
     }
